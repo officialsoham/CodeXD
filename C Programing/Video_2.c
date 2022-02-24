de73032f3add0d02e4176ea324bf45c9c85e66da
officialsoham <96002274+officialsoham@users.noreply.github.com>
2022-02-24 17:49:41 +0530

    Add files via upload

---
+++ C Programing/Video_2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	printf("Subscribe to Code XD");
+	system("COLOR F1");
+	
+	printf("\n\nPress any key For change Theme");
+	getch();
+	system("COLOR 1F");
+	
+	printf("\n\nPress any key For Change Theme");
+	getch();
+	system("COLOR 0A");
+}
